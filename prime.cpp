34a58a69e8640f83e60a382d29e1dabde0e52a1c
Anusha Talagapu <148945596+anushaGIT30@users.noreply.github.com>
2024-04-24 21:31:48 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -1,29 +1,24 @@
-#include<iostream>
-
-#include <math.h>
+#include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    cin>>n;
-int count=0;
-for(int i=1;i<=sqrt(n);i++){
-    //for(int i=1;i*i<=n;i++)
-    if(n%i==0){
-        count=count+1;
+    cin >> n;
+    int count = 0;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            // If i is a divisor of n, increment count
+            count++;
+            // If i * i is not equal to n, then n/i is also a divisor of n, so increment count
+            if (i * i != n) {
+                count++;
+            }
+        }
     }
-    if(n%i!=i){
-        count=count+1;
-        
+    if (count == 2) {
+        cout << "prime";
+    } else {
+        cout << "not";
     }
-    if(count>2){
-        break;
-    }
-    if(count==2){
-        cout<<"is a prime"<<endl;
-    }
-    else
-    cout<<"not a prime"<<endl;
-
-}
-
+    return 0;
 }
