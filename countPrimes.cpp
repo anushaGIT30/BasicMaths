0534e95bce0bc650631878fac47a25a94f761be5
Anusha Talagapu <anushatalagapu@gmail.com>
2024-05-03 15:53:26 +0530

    counting the primes between a given range

--- countPrimes.cpp
+++ countPrimes.cpp
@@ -26,6 +26,7 @@ using namespace std;
     int main(){
         int n;
         cin>>n;
+        
         int r=countPrimes(n);
         cout<<r;
     }
---
+++ primes_counting.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int counting(int n, int left, int right) {
+    int count = 0;
+    int prime[n+1];
+    
+    for(int i = 2; i <= n; i++) {
+        prime[i] = 1;
+    }
+    
+    for(int i = 2; i * i <= n; i++) {
+        if(prime[i] == 1) {
+            for(int j = i * i; j <= n; j += i) {
+                prime[j] = 0;
+            }
+        }
+    }
+    
+    for(int i = left; i <= right; i++) {
+        if(prime[i] == 1) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+int main() {
+    int test_cases;
+    cout << "Enter the number of test cases: ";
+    cin >> test_cases;
+    
+    while(test_cases--) {
+        int l, r;
+        cout << "Enter the range [left, right]: ";
+        cin >> l >> r;
+        
+        int res = counting(r, l, r);
+        cout << "Number of prime numbers between " << l << " and " << r << ": " << res << endl;
+    }
+    
+    return 0;
+}
+
